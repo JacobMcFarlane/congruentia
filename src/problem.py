1d0bd9b1fe1a3abb0ddd604835df6f77d2d9b349
jacobmcf <jacobmcf@student.ubc.ca>
2023-11-20 18:26:03 -0700

    implement get_example_problem, record_response

--- src/problem.py
+++ src/problem.py
@@ -1,4 +1,6 @@
-# class problem
+import random
+
+
 class Problem:
     def __init__(self, problem_type, review_history, problem_examples, next_review):
         self.problem_type = problem_type
@@ -8,15 +10,35 @@ class Problem:
 
     def review_problem(self):
         """Performs a review of a problem"""
+        example_problem = get_example_problem()
+        # Display prompt
+        # Ask for keypress to display image of solution
+        # Then ask for a y/n on whether response was correct
+        # Save that response
         pass
 
     def get_example_problem(self):
         """Gets a problem number to display for review"""
-        pass
+        review_history = [review["problem_ex_id"] for review in self.review_history]
+
+        available_examples = [
+            problem["problem_ex_id"] for problem in self.problem_examples
+        ]
+
+        unused_examples = list(set(available_examples) - set(review_history))
+
+        # if any of the examples haven't been used yet, we return one of those examples
+        if len(unused_examples) > 0:
+            return self.problem_examples[unused_examples[0]]
+
+        # Grab a random problem otherwise
+        return self.problem_examples[random.choice(available_examples)]
 
     def record_response(self, date, is_correct, problem):
         """Transforms user response into an entry in review history"""
-        pass
+        self.review_history.append(
+            {"date": date, "is_correct": is_correct, "problem_ex_id": problem}
+        )
 
     def update_next_review(self):
         """Based on review history, updates next review date."""
