fc1fe3af096632c011bd4a0f175b1f755817e5f1
jacobmcf <jacobmcf@student.ubc.ca>
2023-11-20 18:27:03 -0700

    change indexing in example problem

--- src/test_problem.py
+++ src/test_problem.py
@@ -12,16 +12,18 @@ def problem_from_json(json_path):
 def addition_problem():
     problem_dict = {
         "problem_type": "addition",
-        "review_history": [{"date": "20/11/2023", "is_correct": True, "problem": 1}],
+        "review_history": [
+            {"date": "20/11/2023", "is_correct": True, "problem_ex_id": 1}
+        ],
         "next_review": "11/21/2023",
         "problem_examples": [
             {
-                "problem_example_id": 1,
+                "problem_ex_id": 0,
                 "prompt_path": "add1.png",
                 "solution_path": "add1s.png",
             },
             {
-                "problem_example_id": 2,
+                "problem_ex_id": 1,
                 "prompt_path": "add2.png",
                 "solution_path": "add2s.png",
             },
@@ -38,7 +40,7 @@ def addition_problem():
 
 
 def test_get_example_problem(addition_problem):
-    assert addition_problem.get_example_problem() == 2
+    assert addition_problem.get_example_problem()["problem_ex_id"] == 0
 
 
 def test_record_response(addition_problem):
@@ -46,5 +48,5 @@ def test_record_response(addition_problem):
     assert addition_problem.review_history[-1] == {
         "date": "19/1/2030",
         "is_correct": True,
-        "problem": 8,
+        "problem_ex_id": 8,
     }
