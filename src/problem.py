e3ae04c1f474a7340e563b167a205d42c3a862c1
jacobmcf <jacobmcf@student.ubc.ca>
2023-11-20 17:53:33 -0700

    add new inputs to record response

--- src/problem.py
+++ src/problem.py
@@ -3,30 +3,23 @@ class Problem:
     def __init__(self, problem_type, review_history, problem_examples, next_review):
         self.problem_type = problem_type
         self.review_history = review_history
-        self.problem_examples = problem_examples 
+        self.problem_examples = problem_examples
         self.next_review = next_review
 
     def review_problem(self):
-        """ Performs a review of a problem
-        """
+        """Performs a review of a problem"""
         pass
 
     def get_example_problem(self):
-        """ Gets a problem number to display for review
-        """
+        """Gets a problem number to display for review"""
         pass
 
-    def record_response(self):
-        """ Transforms user response into an entry in review history
-        """
+    def record_response(self, date, is_correct, problem):
+        """Transforms user response into an entry in review history"""
         pass
 
     def update_next_review(self):
-        """Based on review history, updates next review date. 
-        """
-        #TODO implement spaced repition algo
-        
+        """Based on review history, updates next review date."""
+        # TODO implement spaced repition algo
+
         pass
-
-    
-
