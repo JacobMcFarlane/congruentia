15078a37378fdee016b869ba8c37b266730ba8bf
jacobmcf <jacobmcf@student.ubc.ca>
2023-11-25 07:16:39 -0700

    implement basic functions, remove card params

--- src/congruentia/subject.py
+++ src/congruentia/subject.py
@@ -1,10 +1,11 @@
+from datetime import date, datetime, timedelta
+
+
 class Subject:
     review_list = []
 
-    def __init__(self, problems, max_new_cards, max_review_cards):
+    def __init__(self, problems):
         self.problems = problems
-        self.max_new_cards = max_new_cards
-        self.max_review_cards = max_review_cards
 
     def review_subject(self):
         self.generate_review_list()
@@ -13,16 +14,19 @@ class Subject:
             problem.review_problem()
             self.update_review_list()
 
-        # Determine which cards should be reviewed
-        # Display a card from num_new_cards, else get a new problem
-        # pop a problem from review list,
-        # Determine if card should be added to or removed from problems
-        pass
-
-    def generate_review_list(self):
+    def generate_review_list(self, date_to_check):
         """Constructs initial ordering of review in review list"""
-        pass
 
-    def update_review_list(self):
+        self.review_list = [
+            problem
+            for problem in self.problems
+            if datetime.strptime(self.next_review, self.date_format) <= date_to_check
+        ]
+
+    def update_review_list(self, date_to_check):
         """Reorder list after a review based on updated review dates"""
-        pass
+        self.review_list = [
+            problem
+            for problem in self.review_list
+            if datetime.strptime(self.next_review, self.date_format) <= date_to_check
+        ]
