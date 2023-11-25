6d01014ecebbea608c1806db8a5a1efac738344c
jacobmcf <jacobmcf@student.ubc.ca>
2023-11-25 07:01:34 -0700

    add future problem and test for review list

--- tests/test_subject.py
+++ tests/test_subject.py
@@ -13,3 +13,18 @@ def addition_problem():
 @pytest.fixture
 def subtraction_problem():
     return problem_from_json("problem_jsons/example_problem_2.json")
+
+
+@pytest.fixture
+def future_problem():
+    return problem_from_json("problem_jsons/example_problem_future.json")
+
+
+@pytest.fixture
+def basic_math_subject(addition_problem, subtraction_problem):
+    subject.Subject([addition_problem, subtraction_problem], 3, 3)
+
+
+def test_generate_review_list(basic_math_subject):
+    basic_math_subject.generate_review_list()
+    assert len(basic_math_subject.review_list) == 2
