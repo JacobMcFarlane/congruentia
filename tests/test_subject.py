d7471b86754b6b8095478c028e792ae9907268cf
jacobmcf <jacobmcf@student.ubc.ca>
2023-11-25 07:39:49 -0700

    fix error in dates

--- tests/test_subject.py
+++ tests/test_subject.py
@@ -1,5 +1,5 @@
 import pytest
-
+from datetime import datetime, date
 from congruentia import problem
 from congruentia import subject
 from congruentia.utils import problem_from_json
@@ -22,21 +22,27 @@ def future_problem():
 
 @pytest.fixture
 def basic_math_subject(addition_problem, subtraction_problem, future_problem):
-    return subject.Subject(
-        [addition_problem, subtraction_problem, future_problem], 3, 3
-    )
+    return subject.Subject([addition_problem, subtraction_problem, future_problem])
 
 
-def test_generate_review_list(basic_math_subject):
-    basic_math_subject.generate_review_list()
+@pytest.fixture
+def past_date():
+    return datetime.strptime("2024-01-11", "%Y-%m-%d")
+
+
+def test_generate_review_list(basic_math_subject, past_date):
+    basic_math_subject.generate_review_list(past_date)
     assert len(basic_math_subject.review_list) == 2
 
 
 def test_update_review_list(
-    basic_math_subject, addition_problem, subtraction_problem, future_problem
+    basic_math_subject, addition_problem, subtraction_problem, future_problem, past_date
 ):
     basic_math_subject.review_list = [
-        [addition_problem, subtraction_problem, future_problem]
+        addition_problem,
+        subtraction_problem,
+        future_problem,
     ]
-    basic_math_subject.update_review_list()
+
+    basic_math_subject.update_review_list(past_date)
     assert len(basic_math_subject.review_list) == 2
